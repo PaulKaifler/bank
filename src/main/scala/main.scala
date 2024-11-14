b81bba3cdc06a599471c6bd99a8da6bcb5051686
Paul Kaifler <paul@kaifler.eu>
2024-11-14 09:55:16 +0100

    Use cents for balance

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,26 +1,30 @@
 @main
 def main() = {
-  class SimpleAccount(var balance: Double) extends Account
+  class SimpleAccount(var balance: Int) extends Account
 
   // Example account 1: Alice's account
-  val aliceAccount = new SimpleAccount(75.50) // Balance in EUR
-  println(s"Alice's initial balance: ${aliceAccount.balance} EUR")
+  val aliceAccount = new SimpleAccount(7550) // Balance in EUR
+  println(s"Alice's initial balance: ${aliceAccount.getBalanceInEur()} EUR")
 
-  aliceAccount.deposit(20.0)
-  println(s"Alice's balance after deposit: ${aliceAccount.balance} EUR")
+  aliceAccount.deposit(200)
+  println(s"Alice's balance after deposit: ${aliceAccount.getBalanceInEur()} EUR")
 
-  aliceAccount.withdraw(19.75)
-  println(s"Alice's balance after withdrawal: ${aliceAccount.balance} EUR")
+  aliceAccount.withdraw(1975)
+  println(s"Alice's balance after withdrawal: ${aliceAccount.getBalanceInEur()} EUR")
 }
 
 trait Account {
-  var balance: Double // Balance in EUR
+  var balance: Int // Balance in EUR-Cents
 
-  def deposit(amount: Double): Unit = {
+  def getBalanceInEur(): Double = {
+    balance / 100
+  }
+
+  def deposit(amount: Int): Unit = {
     balance += amount
   }
 
-  def withdraw(amount: Double): Unit = {
+  def withdraw(amount: Int): Unit = {
     if (balance >= amount) balance -= amount
     else println("Insufficient funds")
   }
