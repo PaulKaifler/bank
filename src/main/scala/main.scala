547936c688ac66f26b1f55f7661b26873e6e23a6
Paul Kaifler <paul@kaifler.eu>
2024-11-14 09:46:14 +0100

    Trait Account

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,17 @@
 @main
 def main() = {
-  println("Hello world")
 
+}
+
+trait Account {
+  var balance: Double // Balance in EUR
+
+  def deposit(amount: Double): Unit = {
+    balance += amount
+  }
+
+  def withdraw(amount: Double): Unit = {
+    if (balance >= amount) balance -= amount
+    else println("Insufficient funds")
+  }
 }
